00c17705e533866be5a14702fe5380b56ec65c43
Mohamed Mawed Abdel Aleem <mohmmedmoued55@gmail.com>
2020-03-06 23:50:52 +0200

    backendAPI editing note list api and add note create api

--- backend/notes/urls.py
+++ backend/notes/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
-from .views import CreateNote, NoteRUD, NoteList
+from .views import  NoteRUD, NoteListCreate
 urlpatterns = [
-    url(r'^note/$', NoteList.as_view(), name='note_list'),
-    url(r'^note/(?P<pk>\d+)/$', NoteList.as_view(), name='note_RUD'),
-    url(r'^note/$', NoteList.as_view(), name='note_create'),
+    url(r'^note/$', NoteListCreate.as_view(), name='note_list'),
+    url(r'^note/(?P<pk>\d+)/$', NoteRUD.as_view(), name='note_RUD'),
 ]
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -5,16 +5,14 @@ from rest_framework import generics
 from .models import Note
 from rest_framework.permissions import BasePermission, IsAuthenticated
 from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
+
 
 # for filtering the user manager from being able to control the other the notes
 class NotUserManager(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_staff or (not request.user.is_user_manager) 
 
-class CreateNote(generics.CreateAPIView):
-    queryset = Note.objects.all()
-    serializer_class = NoteSerializer
-    permission_classes = (IsAuthenticated,NotUserManager,) 
 
 
 class NoteRUD(generics.RetrieveUpdateDestroyAPIView):
@@ -23,12 +21,25 @@ class NoteRUD(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,NotUserManager,) 
 
 
-class NoteList(generics.ListAPIView):
+class NoteListCreate(generics.ListCreateAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
     permission_classes = (IsAuthenticated,NotUserManager,) 
     def list(self, request):
-        # Note the use of `get_queryset()` instead of `self.queryset`
         queryset = Note.objects.filter(owner=request.user)
         serializer = NoteSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request):
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            note, created = Note.objects.get_or_create(
+                note=serializer.validated_data['note'],
+                start=serializer.validated_data['start'],
+                end=serializer.validated_data['end'],
+                owner=request.user,
+            )
+            if not created:
+                return JsonResponse({'details': "Note name already exist."}, status=400)
+            return JsonResponse({'message': "Note Created Successfully"}, status=201)
+        return JsonResponse({'details': "Invalid Note."}, status=400)
